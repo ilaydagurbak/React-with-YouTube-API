e760d3941b43a5f605d390cb909c7c6be5724b84
ilaydagurbak <ilaydagurbak@gmail.com>
2018-09-19 08:19:03 +0300

    fetch_date

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
-const App = function() {
-    return <div>Hi!</div>;
+import SearchBar from './components/search_bar'
+
+//most parent component should fetch the data
+const API_KEY = 'AIzaSyB-gjN0ImLaAez19QcbCHmnt7wc208Ijhs'
+
+YTSearch({key: API_KEY , term: 'bulldogs'}, function(data) {
+    console.log(data)
+})
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'))
