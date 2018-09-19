3d2511b305d388a669b70e1d4e8e2e13a1add2ab
ilaydagurbak <ilaydagurbak@gmail.com>
2018-09-19 18:29:51 +0300

    video_select

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
-import VideoDetail from './components/video_details'
+import VideoDetail from './components/video_detail'
 
 //most parent component should fetch the data
 const API_KEY = 'AIzaSyB-gjN0ImLaAez19QcbCHmnt7wc208Ijhs'
@@ -12,11 +12,16 @@ class App extends Component {
     constructor(props) {
         super(props)
 
-        this.state = { videos: [] }
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+         }
 
         YTSearch({key: API_KEY , term: 'bulldog puppies'}, (videos) => {
-           this.setState({ videos })
-           //this.setState({ videos: videos })
+           this.setState({ 
+               videos: videos,
+               selectedVideo: videos[0]
+             })
         })
     }
 
@@ -24,8 +29,10 @@ class App extends Component {
     return (
         <div>
             <SearchBar />
-            <VideoDetail video={this.state.videos[0]}/>
-            <VideoList videos={this.state.videos} />
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList 
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos={this.state.videos} />
         </div>
     )
   }
