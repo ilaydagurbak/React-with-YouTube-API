cc33919622ff961e2818cd1b7aa9b1435970cb79
ilaydagurbak <ilaydagurbak@gmail.com>
2018-09-19 16:17:12 +0300

    video_display

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_details'
 
 //most parent component should fetch the data
 const API_KEY = 'AIzaSyB-gjN0ImLaAez19QcbCHmnt7wc208Ijhs'
 
-YTSearch({key: API_KEY , term: 'bulldogs'}, function(data) {
-    console.log(data)
-})
+class App extends Component {
+    constructor(props) {
+        super(props)
 
-const App = () => {
+        this.state = { videos: [] }
+
+        YTSearch({key: API_KEY , term: 'bulldog puppies'}, (videos) => {
+           this.setState({ videos })
+           //this.setState({ videos: videos })
+        })
+    }
+
+    render() {
     return (
         <div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]}/>
+            <VideoList videos={this.state.videos} />
         </div>
     )
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'))
