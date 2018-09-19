747b07584b2ebc54b0ce0494721815477b46a11c
ilaydagurbak <ilaydagurbak@gmail.com>
2018-09-19 20:29:14 +0300

    style+search optimization

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
@@ -16,19 +17,24 @@ class App extends Component {
             videos: [],
             selectedVideo: null
          }
+         this.videoSearch('frenchbulldog puppies')
+    }
 
-        YTSearch({key: API_KEY , term: 'bulldog puppies'}, (videos) => {
-           this.setState({ 
-               videos: videos,
-               selectedVideo: videos[0]
-             })
-        })
+    videoSearch(term) {
+        YTSearch({key: API_KEY , term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+              })
+         })
     }
 
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
     return (
         <div>
-            <SearchBar />
+            <SearchBar onSearchTermChange={videoSearch}/>
             <VideoDetail video={this.state.selectedVideo}/>
             <VideoList 
             onVideoSelect={selectedVideo => this.setState({selectedVideo})}
